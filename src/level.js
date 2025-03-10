00d4faefbbd96f973ecc0f4369cefb97cf008caa
Dogika <59515291+Dogika@users.noreply.github.com>
2025-03-10 07:48:10 +0100

    Create level.js

---
+++ src/level.js
@@ -0,0 +1,15 @@
+/*
+after enemies are spawned a listener is created and added to g_listeners
+checks if there are no more enemies
+if so, spawn next wave of enemies or leave combat
+
+spawn wave of enemies:
+
+a list of spawn enemy functions are created and stored in a list
+this is a wave
+
+current time is added to the timestamp of each event when created
+for sequential spawning
+
+
+*/
